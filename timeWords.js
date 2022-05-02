b690f1874926a13ccc9937ac99d6bc25e741a7ad
Serena Sorensen <serena.sorensen7@gmail.com>
2022-05-02 10:24:50 -0600

    added comments and 0 being 12

--- timeWords.js
+++ timeWords.js
@@ -1,5 +1,6 @@
 function timeWord(time) {
   let numbers = {
+    0: "twelve", //if hour is 0 it is twelve am;
     1: "one",
     2: "two",
     3: "three",
@@ -24,28 +25,36 @@ function timeWord(time) {
     40: "forty",
     50: "fifty",
   };
+  //split time into hour and minute
   let hour = +time.slice(0, 2);
   let minute = +time.slice(3, 5);
+  //check for case of midnight and noon
   if (hour === 0 && minute === 0) {
     return "midnight";
   } else if (hour === 12 && minute === 0) {
     return "noon";
   }
+  //set the return string to be empty;
   let timeString = "";
+  //handle the hour return number between 0 and 12;
   if (hour > 12) {
     timeString += numbers[hour - 12];
   } else {
     timeString += numbers[hour];
   }
+  //handle minutes
   if (minute < 10) {
+    //if minute under 10 handle o options
     if (minute === 0) {
       timeString += " o'clock";
     } else {
       timeString += " oh " + numbers[minute];
     }
   } else if (minute < 20) {
+    //if minute under 20 handle teens
     timeString += " " + numbers[minute];
   } else {
+    //if above or equal to 20 handle tens and ones places
     if (minute % 10 === 0) {
       timeString += " " + numbers[minute];
     } else {
@@ -53,6 +62,7 @@ function timeWord(time) {
         " " + numbers[minute - (minute % 10)] + " " + numbers[minute % 10];
     }
   }
+  //check am or pm
   if (hour >= 12) {
     timeString += " pm";
   } else {
@@ -61,4 +71,4 @@ function timeWord(time) {
   return timeString;
 }
 
-console.log(timeWord("23:23"));
+console.log(timeWord("00:23"));
